86251e754ccc23e21d8cf473876805fcfcccd762
Ivan Geevo <dbowgaming@gmail.com>
2024-04-24 15:17:08 +0300

    many changes //

---
+++ src/main/java/btwr/core/mixin/block/BlockMixin.java
@@ -0,0 +1,28 @@
+package btwr.core.mixin.block;
+
+import btwr.core.block.BlockManager;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Block.class)
+public abstract class BlockMixin
+{
+
+    @Inject(method = "dropStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)V", at = @At("HEAD"), cancellable = true)
+    private static void injectedDropStacks(BlockState state, World world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack tool, CallbackInfo ci)
+    {
+        BlockManager.dropStacksInDirectionOr(state, world, pos, blockEntity, entity, tool);
+        ci.cancel();
+    }
+
+
+}
