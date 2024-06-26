68f3f54420681a6843c6b275758aa75f06b548c6
Ivan Geevo <dbowgaming@gmail.com>
2024-06-26 22:12:18 +0300

    // bump

--- src/main/java/btwr/core/mixin/item/BucketItemMixin.java
+++ src/main/java/btwr/core/mixin/item/BucketItemMixin.java
@@ -1,24 +1,34 @@
 package btwr.core.mixin.item;
 
 import btwr.core.util.MiscUtils;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidDrainable;
 import net.minecraft.block.FluidFillable;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
-import net.minecraft.item.BucketItem;
-import net.minecraft.item.FluidModificationItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -38,7 +48,6 @@ public abstract class BucketItemMixin extends Item implements FluidModificationI
     public BucketItemMixin(Settings settings) {
         super(settings);
     }
-
 
     @Inject(method = "placeFluid", at = @At("HEAD"), cancellable = true)
     private void modifyPlaceFluid(PlayerEntity player, World world, BlockPos pos, BlockHitResult hitResult, CallbackInfoReturnable<Boolean> cir) {
