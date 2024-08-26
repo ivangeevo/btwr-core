89199265ced735150cc01fb9fa19b3628814f020
Ivan Geevo <dbowgaming@gmail.com>
2024-08-26 21:24:08 +0300

    code cleanup

--- src/main/java/btwr/core/mixin/entity/PlayerEntityMixin.java
+++ src/main/java/btwr/core/mixin/entity/PlayerEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,9 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin implements PlayerEntityAdded
 {
-
-
-    private int timesCraftedThisTick = 0;
+    @Unique private int timesCraftedThisTick = 0;
 
     @Override
     public int timesCraftedThisTick() {
