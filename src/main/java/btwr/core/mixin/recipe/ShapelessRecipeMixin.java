9a6803bdb128acd9e12467e8262b92958ffad008
Ivan Geevo <dbowgaming@gmail.com>
2024-09-04 05:49:36 +0300

    fixed extended shapeless recipe serializer additional drops not dropping properly

--- src/main/java/btwr/core/mixin/recipe/ShapelessRecipeMixin.java
+++ src/main/java/btwr/core/mixin/recipe/ShapelessRecipeMixin.java
@@ -4,26 +4,15 @@ import btwr.core.recipe.interfaces.ShapelessRecipeAdded;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.ShapelessRecipe;
-import net.minecraft.recipe.book.CraftingRecipeCategory;
 import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ShapelessRecipe.class)
 public abstract class ShapelessRecipeMixin implements ShapelessRecipeAdded
 {
     @Unique
     DefaultedList<ItemStack> additionalDrops;
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void injectedConstructor(String group, CraftingRecipeCategory category, ItemStack result, DefaultedList<ShapelessRecipe> ingredients, CallbackInfo ci)
-    {
-        this.additionalDrops = getAdditionalDrops();
-    }
-
 
     @Override
     public DefaultedList<ItemStack> getAdditionalDrops() {
